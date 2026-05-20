9d9eefc658dd9ab7e1d66589cbaffb7c3397306d
hasad55fgh <hasad55fgh@icloud.com>
2026-05-21 00:38:40 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "xyz.appmaker.pbyvul"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "2.5.0"
+        versionCode = 16
+        versionName = "2.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
